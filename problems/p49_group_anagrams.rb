764e86c7db5a4dd1e126c6e4b64487893591b8b1
mashiro <mail@mashiro.org>
2019-12-19 16:36:39 +0900

    sort して join したほうが速い

--- problems/p49_group_anagrams.rb
+++ problems/p49_group_anagrams.rb
@@ -12,5 +12,14 @@ def hash(str)
   str.chars.sort.join
 end
 
+def hash2(str)
+  a = Array.new(26, 0)
+  n = 'a'.ord
+  str.chars.each do |c|
+    a[c.ord - n] += 1
+  end
+  '#' + a.join('#')
+end
+
 p group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
 p group_anagrams(["cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc"])
